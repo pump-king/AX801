8f3f067d23f13b219ac5a41feae27fe7741006db
pump-king <merlin.yao@gmail.com>
2020-03-28 12:03:33 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x = 0,y = 10;
+    char ascii = 'A';
+
+y = 1+(8%2==0);
+//A = 65
+
+    printf("%d\n",y);
+    system("pause");
+    return 0;
+}
